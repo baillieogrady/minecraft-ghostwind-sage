de02a5a3bf0cc6c7573832911a670127df19d3f0
Baillie O'Grady <baillieogrady@Baillies-iMac.local>
2020-04-04 11:57:46 +0100

    Add overflow to prevent horizontal scrollung

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -63,7 +63,7 @@
     </div>
     <!--/lead card-->
     @endforeach
-    <div class="bg-gray-200 w-full text-xl md:text-2xl text-gray-800 leading-normal rounded-t">
+    <div class="bg-gray-200 w-full text-xl md:text-2xl text-gray-800 leading-normal rounded-t overflow-hidden">
       
       <!-- post container-->
       <div class="flex flex-wrap justify-between pt-12 -mx-6">
